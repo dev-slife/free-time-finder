26955dac6acba2085066b235b389b6417719d12b
dev.slife <smallfrycode@gmail.com>
2026-02-12 14:18:29 -0500

    Found and fixed the weird directory issue.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-export function changePage(page="home") {
-    const base_url = "../pages/"
+export function changePage(page="home", is_index_file=false) {
+    const base_url = (is_index_file) ? "./pages/": "../pages/";
     const file_name = (page.includes(".html")) ? page: page+".html";
     const url = base_url + file_name;
     window.location.href = url;
